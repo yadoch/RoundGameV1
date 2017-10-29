6c95fb553a7bb8a07fca0110889ab78ecd011cc4
yadoch <yadoch@gmail.com>
2017-10-29 21:16:25 +0800

    加入行動力 ep 機制

--- app/src/main/java/tw/com/abc/roundgame/MainActivity.java
+++ app/src/main/java/tw/com/abc/roundgame/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTvRound,mTvResource1,mTvTotUnit,mTvTotLand;
-    private int mRound,mResource1,mTotUnit,mTotLand;
+    private TextView mTvRound,mTvResource1,mTvTotUnit,mTvTotLand,mTvEp;
+    private int mRound,mResource1,mTotUnit,mTotLand,intEp;
     private int newRoundResource1,useRound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +21,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public  void addLand(View v){
-        mTotLand +=1;
-        //畫面更新
-        renewDisplay();
-        Toast.makeText(this,"Land add:1",Toast.LENGTH_SHORT).show();
+        if(intEp > 0) {
+            mTotLand += 1;
+            intEp -= 1;
+            //畫面更新
+            renewDisplay();
+            Toast.makeText(this, "Land add:1", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"No Executive Power,Please End Round!!",Toast.LENGTH_SHORT).show();
+        }
     }
 
     public  void addUnit(View v){
         //每按一下新增單位
-        mTotUnit += 1;
-        //畫面更新
-        renewDisplay();
-        Toast.makeText(this,"The Unit add:1",Toast.LENGTH_SHORT).show();
+        if(intEp > 0){
+            mTotUnit += 1;
+            intEp -=1;
+            //畫面更新
+            renewDisplay();
+            Toast.makeText(this,"The Unit add:1",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this,"No Executive Power,Please End Round!!",Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 
     public void endRound(View v){
-
-        //每回合新增資源計算
-        countResource();
-        mRound +=1;
-        //畫面更新
-        renewDisplay();
-        Toast.makeText(this,"The "+ mRound  +" Round,Food add:"+ newRoundResource1+";Unit use :"+useRound ,Toast.LENGTH_SHORT).show();
+        if(mTotLand < 6) {
+            //每回合新增資源計算
+            countResource();
+            mRound += 1;
+            intEp = 2;
+            //畫面更新
+            renewDisplay();
+            Toast.makeText(this, "The " + mRound + " Round,Food add:" + newRoundResource1 + ";Unit use :" + useRound, Toast.LENGTH_SHORT).show();
+        }else {Toast.makeText(this, "You Win!! Game Over!!", Toast.LENGTH_SHORT).show();}
     }
     //每回合新增資源計算
     private void countResource() {
@@ -57,11 +70,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         mRound =0;
+        intEp=2;
         mResource1=0;
         mTotUnit=0;
         mTotLand=1;
 
         mTvRound= (TextView) findViewById(R.id.tv_round);
+        mTvEp=(TextView)findViewById(R.id.tv_ep);
         mTvResource1= (TextView) findViewById(R.id.tv_resource1);
         mTvTotUnit= (TextView) findViewById(R.id.tv_unit1);
         mTvTotLand= (TextView) findViewById(R.id.tv_land);
@@ -70,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
     //畫面更新
     private void renewDisplay() {
         mTvRound.setText(String.valueOf(mRound));
+        mTvEp.setText(String.valueOf(intEp));
         mTvResource1.setText(String.valueOf(mResource1));
         mTvTotLand.setText(String.valueOf(mTotLand));
         mTvTotUnit.setText(String.valueOf(mTotUnit));
